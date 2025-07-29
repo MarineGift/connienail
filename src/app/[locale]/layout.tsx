c37abeaa537d34396f3a3b42d58e9bdf5455804d
MarineGift <marinegift4u@gmail.com>
2025-07-29 16:07:38 -0400

    1

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from 'react';
 import '../../styles/globals.css';
 
-type Props = {
+export default function LocaleLayout({
+  children,
+  params,
+}: {
   children: ReactNode;
   params: { locale: string };
-};
-
-export default function LocaleLayout({ children, params }: Props) {
+}): JSX.Element {
   return (
     <html lang={params.locale}>
       <body>{children}</body>
