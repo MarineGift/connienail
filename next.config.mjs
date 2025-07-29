36884e4330eb0c5ef37b893d4b43d4cba5291f80
MarineGift <marinegift4u@gmail.com>
2025-07-29 00:49:10 -0400

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'ko'],
-    defaultLocale: 'en',
-  },
 };
 
 export default nextConfig;
