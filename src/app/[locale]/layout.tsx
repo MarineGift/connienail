ba5863c4c3baff344adb23e9eedf6391c611b6a3
MarineGift <marinegift4u@gmail.com>
2025-07-29 16:04:45 -0400

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,12 @@
 import { ReactNode } from 'react';
 import '../../styles/globals.css';
 
-export default function LocaleLayout({
-  children,
-  params,
-}: {
+type Props = {
   children: ReactNode;
   params: { locale: string };
-}) {
+};
+
+export default function LocaleLayout({ children, params }: Props) {
   return (
     <html lang={params.locale}>
       <body>{children}</body>
