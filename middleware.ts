6631f2c13b638ffa425f40de2cc23064baf465ed
MarineGift <138279911+MarineGift@users.noreply.github.com>
2025-07-29 03:59:31 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,11 @@
 import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
-  // A list of all locales that are supported
   locales: ['ko', 'en', 'ja', 'es'],
-
-  // Used when no locale matches
   defaultLocale: 'ko',
-
-  // Always use locale prefix
-  localePrefix: 'always'
+  localePrefix: 'as-needed'
 });
 
 export const config = {
-  // Match only internationalized pathnames
   matcher: ['/', '/(ko|en|ja|es)/:path*']
 };
