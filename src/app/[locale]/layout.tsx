69a43b940312332d76e76b206d731f501ed1c189
MarineGift <marinegift4u@gmail.com>
2025-07-29 01:01:31 -0400

    a

--- src/app/[locale]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import '../globals.css'
-
-export default async function LocaleLayout({ children, params }) {
-  return (
-    <html lang={params.locale}>
-      <body>{children}</body>
-    </html>
-  )
-}
