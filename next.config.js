9b8de139e8c3ac525480b4f82080115385d2e2c8
MarineGift <marinegift4u@gmail.com>
2025-07-30 18:17:10 -0400

    a

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const createNextIntlPlugin = require('next-intl/plugin');
-
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'ko'],
+    defaultLocale: 'ko',
+  },
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
