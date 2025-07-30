f8fbbd48540b3eb473d246ce73b4a00f25ea006d
MarineGift <marinegift4u@gmail.com>
2025-07-30 18:41:18 -0400

    a

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,14 +1,32 @@
-import '../../styles/globals.css';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
 import { ReactNode } from 'react';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { notFound } from 'next/navigation';
 
-export default function LocaleLayout({ children }: { children: ReactNode }) {
-  const messages = useMessages();
+type Props = {
+  children: ReactNode;
+  params: { locale: string };
+};
+
+export function generateStaticParams() {
+  return [{ locale: 'en' }, { locale: 'ko' }];
+}
+
+export default async function LocaleLayout({ children, params: { locale } }: Props) {
+  let messages;
+  try {
+    messages = await getMessages({ locale });
+  } catch (error) {
+    notFound();
+  }
 
   return (
-    <html lang="en">
+    <html lang={locale}>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
-        <NextIntlClientProvider messages={messages}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
       </body>
