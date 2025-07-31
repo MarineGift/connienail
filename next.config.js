246bfb8812057e894ea99dbd75fbfaf7442382b6
MarineGift <138279911+MarineGift@users.noreply.github.com>
2025-07-30 23:53:54 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ const withNextIntl = require('next-intl/plugin')('./src/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   experimental: {
     appDir: true,
   },
-  output: 'standalone', // Docker 빌드 최적화
   images: {
     remotePatterns: [
       {
@@ -14,14 +14,6 @@ const nextConfig = {
       },
     ],
   },
-  // 캐시 설정 최적화
-  onDemandEntries: {
-    maxInactiveAge: 25 * 1000,
-    pagesBufferLength: 2,
-  },
-  // 빌드 최적화
-  swcMinify: true,
-  poweredByHeader: false,
 }
 
 module.exports = withNextIntl(nextConfig);
